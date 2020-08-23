cf5fb8bfabd89a4f16d0b23a783efbc95fbbe968
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-23 11:43:14 +0200

    add pagination (#5)

---
+++ src/Application/Common/Pagination/PagedList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Common.Pagination
+{
+    public class PagedList<T>
+    {
+        public IEnumerable<T> Results { get; set; }
+        
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        
+        public int ItemsCount { get; set; }
+        public int TotalItemsCount { get; set; }
+
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+    }
+}
