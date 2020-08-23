5c2ea0c94d835013d11821400db95efe8b0c4dcc
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-23 12:56:56 +0200

    remove changes from another branch

--- src/Application/Startup.cs
+++ src/Application/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Infrastructure;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Application
 {
@@ -33,17 +25,6 @@ namespace Application
             services.AddSpaStaticFiles(options => options.RootPath = "ClientApp/build");
 
             services.AddInfrastructure(Configuration);
-
-            services.AddAuthentication(options =>
-                     {
-                         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                     })
-                    .AddJwtBearer(options =>
-                     {
-                         options.Authority = "https://broad-sun-6187.eu.auth0.com/";
-                         options.Audience = "http://pre-hacknarock.herokuapp.com/api/";
-                     });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,13 +55,6 @@ namespace Application
                 {
                     spa.UseReactDevelopmentServer("start");
                 }
-            });
-
-            app.UseAuthentication();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
