dda2b65a929f2049049570da9a411b9bf3a9c5b8
Bartłomiej Chmiel <bartlomiej.chmiel@dka.io>
2020-08-23 13:30:32 +0200

    Fix

--- src/Application/ClientApp/src/pages/Login/Login.jsx
+++ src/Application/ClientApp/src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Box, TextField, Button, Link } from "@material-ui/core";
+import { Typography, Box, TextField, Link } from "@material-ui/core";
 import styled from "styled-components";
 
 export const Login = () => {
