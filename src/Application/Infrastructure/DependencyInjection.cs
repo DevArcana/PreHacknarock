06f270509b5067de55b380d5b5bc61539feffbea
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-23 10:00:01 +0200

    add boilerplate

---
+++ src/Application/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Application.Infrastructure.Persistance;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ??
+                                       configuration.GetConnectionString("Database");
+
+                options.UseNpgsql(connectionString);
+            });
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            
+            var dbContext = scope.ServiceProvider.GetService<AppDbContext>();
+            dbContext.Database.Migrate();
+
+            return services;
+        }
+    }
+}
