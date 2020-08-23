cca1661f2cd0f3b9a3335bfe334b8c03ded5c659
Bartłomiej Chmiel <bartlomiej.chmiel@dka.io>
2020-08-23 13:20:26 +0200

    Add login page

--- src/Application/ClientApp/src/pages/Login/Login.jsx
+++ src/Application/ClientApp/src/pages/Login/Login.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import { Typography, Box, TextField, Button, Link } from "@material-ui/core";
+import styled from "styled-components";
 
 export const Login = () => {
-    return (<div>Login</div>)
-} 
+  return (
+    <Box m={10}>
+      <Box align={"center"}>
+        <TitleTypography variant="h1">NOTATKI</TitleTypography>
+      </Box>
+      <CredentialsBox>
+        <InputField id="standard-basic" label="username" />
+        <InputField id="standard-basic" label="password" />
+      </CredentialsBox>
+      <ButtonBox>
+        <LoginButton>
+          <Typography variant="h5">Login</Typography>
+        </LoginButton>
+      </ButtonBox>
+    </Box>
+  );
+};
 
+const TitleTypography = styled(Typography)`
+  color: #7c98b3;
+`;
+const CredentialsBox = styled(Box)`
+  margin-top: 70px;
+`;
+
+const InputField = styled(TextField)`
+  width: 100%;
+  height: 30px;
+  margin-bottom: 50px;
+  color: #dbdbdb;
+  font-size: 16;
+`;
+
+const ButtonBox = styled(Box)`
+  display: flex;
+  justify-content: center;
+`;
+
+const LoginButton = styled(Link)`
+  color: #7aa7ec;
+  border-bottom-style: none;
+  text-decoration: none !important;
+`;
