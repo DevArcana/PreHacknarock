a1a1a71e068ff31c440abaaaca5befdfbcbb4592
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-23 09:23:03 +0200

    add test controller

---
+++ src/Application/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("{env}")]
+        public IActionResult GetEnvVar(string env)
+        {
+            return Ok(Environment.GetEnvironmentVariable(env));
+        }
+    }
+}
