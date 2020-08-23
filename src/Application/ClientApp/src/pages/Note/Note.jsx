102622ab63d56dd5baa01152b5b1e31006a77c58
Maciag312 <Maciag312>
2020-08-23 14:14:21 +0200

    "text area added"

--- src/Application/ClientApp/src/pages/Note/Note.jsx
+++ src/Application/ClientApp/src/pages/Note/Note.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { NavBar } from "../../components/NavBar/NavBar";
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 export const Note = () => {
   const { noteId } = useParams();
   return <div>
     <NavBar></NavBar>
     Note {noteId}
+    <br></br>
+    <TextareaAutosize  style={{border: "none", borderColor: "Transparent",  outline: "none", width: "100%"}} aria-label="empty textarea" placeholder="Napisz nową notatkę" />
     </div>;
+
+
 };
