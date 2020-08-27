3aa9b1386904832c6a10ef6179973f1135181bb9
Bartłomiej Chmiel <bachm44@gmail.com>
2020-08-27 22:16:45 +0200

    Final fixes and material ui adjustments

--- src/Application/ClientApp/src/pages/Note/Note.jsx
+++ src/Application/ClientApp/src/pages/Note/Note.jsx
@@ -1,30 +1,44 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 import { useParams } from "react-router-dom";
 import { NavBar } from "../../components/NavBar/NavBar";
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import { Box, Container } from "@material-ui/core";
 
 export const Note = () => {
-
   const { noteId } = useParams();
 
   const [note, setNote] = useState({});
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'http://pre-hacknarock.herokuapp.com/api/rest/notes/'+noteId,
+        `${process.env.REACT_APP_API_URL}rest/notes/` + noteId
       );
       setNote(result.data);
     };
- 
+
     fetchData();
   }, []);
 
-  
-  return <div>
-    <NavBar title={note.title}></NavBar>
-      <br></br>
-      <TextareaAutosize  style={{border: "none", borderColor: "Transparent",  outline: "none", width: "100%"}} aria-label="empty textarea" placeholder="Napisz nową notatkę" defaultValue={note.content}/>
-    </div>;
+  return (
+    <>
+      <NavBar title={note.title} />
+      <Container maxWidth={"sm"}>
+        <Box mt={3}>
+          <TextareaAutosize
+            style={{
+              border: "none",
+              borderColor: "Transparent",
+              outline: "none",
+              width: "100%",
+            }}
+            aria-label="empty textarea"
+            placeholder="Napisz nową notatkę"
+            defaultValue={note.content}
+          />
+        </Box>
+      </Container>
+    </>
+  );
 };
--- src/Application/ClientApp/src/pages/NoteList/NoteItem.jsx
+++ src/Application/ClientApp/src/pages/NoteList/NoteItem.jsx
@@ -1,31 +1,25 @@
 import React from "react";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import {Card, CardContent, Typography} from '@material-ui/core';
 import './NoteItem.css'
-import { useHistory } from "react-router-dom";
-
+import {useHistory} from "react-router-dom";
+import {parseISO, format} from 'date-fns'
 
 export const NoteItem = (props) => {
   const { note } = props;
-
-  let history = useHistory();
-
-
+  const history = useHistory();
+  const createdAt = format(parseISO(note.createdAt), "dd/MM' 'HH:mm:ss")
   const handleClick = () => {
     history.push('/note/'+note.id)
   }
 
   return (
-    <Card className="noteItem" onClick={handleClick}  style={{ marginBottom: "1rem" , cursor: "pointer"}}>
+    <Card className="noteItem" onClick={handleClick}  style={{ marginBottom: "1rem" , cursor: "pointer", minHeight: '5rem'}}>
       <CardContent style={{ paddingBottom: 0 }}>
         <Typography variant="h5" component="h2">
           {note.title}
         </Typography>
         <Typography color="textSecondary">
-          {note.createdAt}
+          {'Last edit at: ' + createdAt}
         </Typography>
       </CardContent>
     </Card>
