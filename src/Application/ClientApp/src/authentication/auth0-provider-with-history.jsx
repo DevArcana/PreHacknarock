b615b733e15e183187a596518f6d2b28356b82b6
Bartłomiej Chmiel <bachm44@gmail.com>
2020-08-27 22:46:05 +0200

    Final fixes and material ui adjustments (#19)

--- src/Application/ClientApp/src/authentication/auth0-provider-with-history.jsx
+++ src/Application/ClientApp/src/authentication/auth0-provider-with-history.jsx
@@ -1,26 +1,18 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN || '';
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
-  const audience = process.env.REACT_APP_AUTH0_AUDIENCE || '';
-  const history = useHistory();
-
-  const onRedirectCallback = (appState) => {
-    history.push( appState && appState.returnTo
-        ? appState.returnTo
-        : window.location.pathname);
-  };
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN || "";
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || "";
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE || "";
+  const redirectUri = process.env.REACT_APP_BASE_URL || "";
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       audience={audience}
-      redirectUri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
+      redirectUri={redirectUri}
     >
       {children}
     </Auth0Provider>
